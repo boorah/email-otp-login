2900524f65153e457a8ce7c8a823179d550a0610
Himanshu Borah <iamhimanshu808@gmail.com>
2025-08-17 17:40:52 +0530

    fix: incorrect error edge case

--- backend/helpers/httpResponse.go
+++ backend/helpers/httpResponse.go
@@ -12,16 +12,17 @@ func RespondWithError(w http.ResponseWriter, requestError RequestError) {
 
 func RespondWithJSON(w http.ResponseWriter, statusCode int, data any) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
 
 	stringifiedResponse, err := json.Marshal(data)
 	if err != nil {
 		log.Println("error while stringifying response:", err)
 
-		w.Write([]byte(`{"type": "internal_server_error", "message": "An internal server error occurred"}`))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"type": "internal_server_error", "statusCode": 500, "message": "An internal server error occurred"}`))
 
 		return
 	}
 
+	w.WriteHeader(statusCode)
 	w.Write(stringifiedResponse)
 }
